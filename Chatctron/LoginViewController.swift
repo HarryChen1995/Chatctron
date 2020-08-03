61a0b503a544600656d86a7ba01ba1abc1dccd00
harrychen <harrychen1995@yahoo.com>
2020-08-03 12:49:03 -0400

    project created

---
+++ Chatctron/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  Chatctron
+//
+//  Created by Hanlin Chen on 8/3/20.
+//  Copyright © 2020 Hanlin Chen. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
